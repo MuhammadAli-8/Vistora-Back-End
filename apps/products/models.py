b7c8eb23f941481c7f244379ae83f9caa9798988
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-12-20 21:33:46 +0200

    Add review model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 class Product(models.Model):
@@ -8,3 +9,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Review {self.review_id} by {self.user.username} for {self.product.name}"
