9d670c348ea740d8be128fd5160f4d1de2fc3261
Taha Ghazi <tahaghazt164@gmail.com>
2024-12-21 01:39:28 +0200

    Add media

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+# ==============================================================================
+# STATIC & MEDIA FILES SETTINGS
+# ==============================================================================
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
