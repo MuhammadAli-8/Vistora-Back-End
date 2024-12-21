25f6543c2df21727cab8301d5cce2bc1c049800a
MuhammadAli-8 <muhammedgerali@gmail.com>
2024-12-21 02:30:29 +0200

    Update Models

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.orders.models import OrderItem, Cart, Shipping
 
 # Register your models here.
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(Shipping)
