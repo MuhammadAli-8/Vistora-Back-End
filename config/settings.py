05f3bf3cb5506f3da7dd35bb8d3bda9da0ce2773
MuhammadAli-8 <muhammedgerali@gmail.com>
2024-12-22 19:29:38 +0200

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -166,16 +166,12 @@ REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
-    # "DEFAULT_AUTHENTICATION_CLASSES": [
-    #     # "rest_framework.authentication.BasicAuthentication",
-    #     "rest_framework.authentication.SessionAuthentication",
-    #     "rest_framework.authentication.TokenAuthentication",
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
