8984cdf3b6f6988c20e1d11fb9a75bafe0a8fcf5
MuhammadAli-8 <muhammedgerali@gmail.com>
2024-12-23 04:38:04 +0200

    Update User model 2

--- apps/users/api/viewsets.py
+++ apps/users/api/viewsets.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from apps.users.api.serializers import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 class UserViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and managing User instances.
